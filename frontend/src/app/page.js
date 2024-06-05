913aa03146017a7a7453b163b52a2b0bab00f807
Yann/Yaming <57467587+yamnmao@users.noreply.github.com>
2024-06-04 22:30:15 -0700

    update

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
 
             <div className='relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start'>
 
-              <div className='absolute w-28 left-0 -top-10 hidden lg:block'>
+              <div className='absolute w-28 left-0 -top-5 hidden lg:block'>
                 <div className='absolute inset-x-0 bottom-0 bg-gradient-to-t via-violet-50/50 from-violet-50 h-28'>
                   <img src='/logo1.png' className='w-full' />
                 </div>
