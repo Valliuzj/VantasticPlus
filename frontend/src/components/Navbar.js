865491403b3c8157ceb793e9285d9b06de908c4d
Yann/Yaming <57467587+yamnmao@users.noreply.github.com>
2024-06-09 22:02:44 -0700

    navbar and layout

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,46 +1,41 @@
 // components/Navbar.js
 import Link from 'next/link';
+import { ScreenWrapper } from './ScreenWrapper';
+import { Button } from './ui/button';
 
 const Navbar = () => {
   return (
-    <nav className="bg-white shadow-md">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16">
-          <div className="flex">
-            <div className="flex-shrink-0">
-              <img className="h-8 w-8" src="/logo1.png" alt="Logo" />
-            </div>
+    <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
+    <ScreenWrapper>
+    <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
             <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
-              <Link href="/">
-                <p className="text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 hover:border-gray-300 focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+              <Link href="/" className='text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 hover:border-gray-300 focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out'>
                   Home
-                </p>
               </Link>
-              <Link href="/study-tools">
-                <p className="text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 hover:border-gray-300 focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+
+              <Link href="/study-tools" className="text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 hover:border-gray-300 focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
                   Study tools
-                </p>
               </Link>
-              <Link href="/subject-areas">
-                <p className="text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 hover:border-gray-300 focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+
+              <Link href="/subject-areas" className="text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 hover:border-gray-300 focus:outline-none focus:border-gray-300 transition duration-150">
                   Subject areas
-                </p>
               </Link>
             </div>
-          </div>
+
           <div className="hidden sm:ml-6 sm:flex sm:items-center">
-            <button className="text-gray-900 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+            <Button className="text-gray-900 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
               Generate
-            </button>
-            <button className="ml-4 text-gray-900 bg-white hover:bg-gray-100 text-sm font-medium py-2 px-4 rounded border border-gray-300">
+            </Button>
+            <Button className="ml-4 text-gray-900 bg-white hover:bg-gray-100 text-sm font-medium py-2 px-4 rounded border border-gray-300">
               Log in
-            </button>
-            <button className="ml-4 text-gray-900 bg-yellow-400 hover:bg-yellow-500 text-sm font-medium py-2 px-4 rounded">
+            </Button>
+            <Button className="ml-4 text-gray-900 bg-yellow-400 hover:bg-yellow-500 text-sm font-medium py-2 px-4 rounded">
               Sign up
-            </button>
+            </Button>
           </div>
+
         </div>
-      </div>
+      </ScreenWrapper>
     </nav>
   );
 };
