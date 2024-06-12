88a34b1526bd7880c0995052f58b7336b51308d1
Ge YANG <syang1221@hotmail.com>
2024-06-12 00:55:18 -0700

    register user api, add password hash and salt, add dotenv

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const registerUserController = require("../controllers/users/registerUser");
 const getSingleUserController = require("../controllers/users/getSingleUser");
 
+router.post("/register", registerUserController.registerUser);
 router.get("/user/:email", getSingleUserController.getSingleUser);
 
 module.exports = router;
