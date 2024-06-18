104777d49b1f388be3bf26f7efa9ae4c73f3e359
Yann/Yaming <57467587+yamnmao@users.noreply.github.com>
2024-06-17 20:50:56 -0700

    update layout

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -8,8 +8,8 @@ export default function Home() {
       <section>
         <ScreenWrapper className='pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32
         lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52'>
+          {/*left part*/}
           <div className='col-span-2 px-6 lg:px-0 lg:pt-4'>
-
             <div className='relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start'>
 
               <div className='absolute w-28 left-0 -top-5 hidden lg:block'>
@@ -23,8 +23,12 @@ export default function Home() {
                 about<span className='bg-rose-500 px-2 mx-2 text-white'>Vancouver</span>{' '}
                 with quiz
               </h1>
-
             </div>
+          </div>
+          {/*right part*/}
+          <div className='col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0
+          mt-32 lg:mx-0 lg:mt-20 h-fit'>
+
           </div>
         </ScreenWrapper>
       </section>
