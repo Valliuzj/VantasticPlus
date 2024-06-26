1fb03b02b4ba7e598a7183ef22d03224feebed0c
Valerie Liu <zijingliu1996@hotmail.com>
2024-06-26 00:57:26 -0700

    get all posts api

--- backend/routes/postsRoutes.js
+++ backend/routes/postsRoutes.js
@@ -5,9 +5,11 @@ const { protect } = require("../middlewares/auth");
 const addPostController = require("../controllers/posts/addPost");
 const addCommentController = require("../controllers/posts/addComment");
 const getSinglePostController = require("../controllers/posts/getSinglePost");
+const getAllPostsController = require("../controllers/posts/getAllPosts");
 
 router.post("/addPost", protect, addPostController.addPost);
 router.post("/addComment", protect, addCommentController.addComment);
 router.get("/getSinglePost/:postID", getSinglePostController.getSinglePost);
+router.get("/getAllPosts", getAllPostsController.getAllPosts);
 
 module.exports = router;
