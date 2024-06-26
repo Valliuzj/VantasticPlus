b2cbd122529479a084f3683c1b572dd48075cf87
Valerie Liu <zijingliu1996@hotmail.com>
2024-06-26 00:21:36 -0700

    get single post api

--- backend/routes/postsRoutes.js
+++ backend/routes/postsRoutes.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const { protect } = require("../middlewares/auth");
 
 const addPostController = require("../controllers/posts/addPost");
+const addCommentController = require("../controllers/posts/addComment");
+const getSinglePostController = require("../controllers/posts/getSinglePost");
 
 router.post("/addPost", protect, addPostController.addPost);
+router.post("/addComment", protect, addCommentController.addComment);
+router.get("/getSinglePost/:postID", getSinglePostController.getSinglePost);
 
 module.exports = router;
