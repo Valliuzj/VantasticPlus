965b81d7e9edd0df20eecfd815327a2c7c6769d5
Ge YANG <syang1221@hotmail.com>
2024-06-27 21:00:04 -0700

    Delete comment API

--- backend/routes/postsRoutes.js
+++ backend/routes/postsRoutes.js
@@ -6,10 +6,12 @@ const addPostController = require("../controllers/posts/addPost");
 const addCommentController = require("../controllers/posts/addComment");
 const getSinglePostController = require("../controllers/posts/getSinglePost");
 const getAllPostsController = require("../controllers/posts/getAllPosts");
+const deleteCommentController = require("../controllers/posts/deleteComment");
 
 router.post("/addPost", protect, addPostController.addPost);
 router.post("/addComment", protect, addCommentController.addComment);
 router.get("/getSinglePost/:postID", getSinglePostController.getSinglePost);
 router.get("/getAllPosts", getAllPostsController.getAllPosts);
+router.delete("/deleteComment", protect, deleteCommentController.deleteComment);
 
 module.exports = router;
