a5bc5d13b658ae898153a584375707b047eb2a09
Ge YANG <syang1221@hotmail.com>
2024-07-11 00:43:56 -0700

    get single question api

--- backend/routes/questionsRoutes.js
+++ backend/routes/questionsRoutes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const { protect } = require("../middlewares/auth");
 
 const addQuestionController = require("../controllers/questions/addQuestion");
+const getQuestionController = require("../controllers/questions/getSingleQuestion");
 
 router.post("/addQuestion", addQuestionController.addQuestion);
+router.get("/getSingleQuestion", getQuestionController.getSingleQuestion);
 
 module.exports = router;
